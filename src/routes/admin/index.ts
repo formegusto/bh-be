d450cde05203b306038086722c0718cdc8599705
formegusto <formegusto@gmail.com>
2021-12-09 12:35:36 +0900

    admin check

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
+import adminCheck from "../middlewares/adminCheck";
 import HumanDataRoutes from "./humanData";
 
 const AdminRoutes = Router();
 
-AdminRoutes.use("/humanData", HumanDataRoutes);
+AdminRoutes.use("/humanData", adminCheck, HumanDataRoutes);
 
 export default AdminRoutes;
---
+++ src/routes/middlewares/adminCheck.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function adminCheck(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const key = req.headers.authorization;
+
+  if (key) {
+    return res.status(401).json({
+      status: false,
+      error: {
+        message: "권한이 없습니다.",
+      },
+    });
+  }
+
+  const adminRequestKey = process.env.ADMIN_REQUEST_KEY;
+  if (key !== adminRequestKey) {
+    return res.status(401).json({
+      status: false,
+      error: {
+        message: "권한이 없습니다.",
+      },
+    });
+  }
+
+  return next();
+}
